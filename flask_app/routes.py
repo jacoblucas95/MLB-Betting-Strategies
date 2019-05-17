b786e65f77012e6fd528bebf6f2aae06794b89d4
Maria Papageorgiou <mpapag1995@gmail.com>
2019-05-17 16:16:39 -0400

    Update routes.py

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -10,28 +10,28 @@ c = CORS(app)
 def root():
 	return jsonify({
 		"game_1":{
-			"home_team":"Home Team1"
-			"away_team":"Away Team1"
-			"location":"City1"
-			"date":"1499817600"
-			"final_score":"69"
-			"odds":"6:9"
-			"open_close_ratio":"9:6"
+			"home_team":"Home Team1",
+			"away_team":"Away Team1",
+			"location":"City1",
+			"date":"1499817600",
+			"final_score":"69",
+			"odds":"6:9",
+			"open_close_ratio":"9:6",
 			"pitcher":{
-				"name":"Mikey Mike"
+				"name":"Mikey Mike",
 				"hand":"R"
 				}
-		}
+		},
 		"game_2":{
-			"home_team":"Home Team2"
-			"away_team":"Away Team2"
-			"location":"City2"
-			"date":"1531353600"
-			"final_score":"420"
-			"odds":"4:20"
-			"open_close_ratio":"4:20"
+			"home_team":"Home Team2",
+			"away_team":"Away Team2",
+			"location":"City2",
+			"date":"1531353600",
+			"final_score":"420",
+			"odds":"4:20",
+			"open_close_ratio":"4:20",
 			"pitcher":{
-				"name":"Bobby Bob"
+				"name":"Bobby Bob",
 				"hand":"L"
 				}
 		}
