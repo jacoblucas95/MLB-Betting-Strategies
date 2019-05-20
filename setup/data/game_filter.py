af147c1f6c12be7718d94e940cfec6d6ed92011c
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-05-20 14:31:27 -0400

    filter inputs

--- setup/data/game_filter.py
+++ setup/data/game_filter.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-import datetime
+from datetime import date
 
 df = pd.read_csv('baseball.csv', low_memory=False)
 
 def date_range(df=df):
+    y1 = int(input('start yr: '))
+    m1 = int(input('start mon: '))
+    d1 = int(input('start day: '))
+
+    y2 = int(input('end yr: '))
+    m2 = int(input('end mon: '))
+    d2 = int(input('end day: '))
+
+    start = date(y1,m1,d1)
+    end = date(y2,m2,d2)
     df['date'] = pd.to_datetime(df['date'])
-    df2 = df[df['date'].isin(pd.date_range('2010-04-05','2010-04-06'))]
+    df2 = df[df['date'].isin(pd.date_range(start,end))]
     return df2
 
 def home_underdogs(df=df):
@@ -15,4 +25,4 @@ def home_underdogs(df=df):
     return df2
 
 if __name__ == '__main__':
-    print(home_underdogs())
+    print(date_range())
