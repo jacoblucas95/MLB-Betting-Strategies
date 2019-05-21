2f902fffa1341cccdf387d6b328643cd09526880
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-05-21 13:27:55 -0400

    flask app

---
+++ run/flask_app/routes.py
@@ -0,0 +1,41 @@
+
+from flask import jsonify,request
+from .run import app
+from app import game
+
+
+@app.route('/')
+def root():
+	return jsonify({
+		"game_1":{
+			"home_team":"Home Team1",
+			"away_team":"Away Team1",
+			"location":"City1",
+			"date":"1499817600",
+			"final_score":"69",
+			"odds":"6:9",
+			"open_close_ratio":"9:6",
+			"pitcher":{
+				"name":"Mikey Mike",
+				"hand":"R"
+				}
+		},
+		"game_2":{
+			"home_team":"Home Team2",
+			"away_team":"Away Team2",
+			"location":"City2",
+			"date":"1531353600",
+			"final_score":"420",
+			"odds":"4:20",
+			"open_close_ratio":"4:20",
+			"pitcher":{
+				"name":"Bobby Bob",
+				"hand":"L"
+				}
+		}
+	})
+
+@app.route('/api/dataset',  methods=['GET','POST'])
+def get_dataset():
+	if request.method == 'GET':
+		return jsonify(game.create_games_generator())
