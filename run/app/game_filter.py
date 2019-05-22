3403eea49454526b6042d490422cc7c663eeebac
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-05-22 10:06:25 -0400

    filtering

--- run/app/game_filter.py
+++ run/app/game_filter.py
@@ -13,5 +13,18 @@ def date_range(df=df, start_date=date(2018,1,1), end_date=date(2019,12,31)):
     df2 = df[df['date'].isin(pd.date_range(start_date,end_date))]
     return df2
 
+def home_away(h_a, df=df):
+    df2 = df.loc(df['visitor_home'] == h_a)
+    return df2
+
+def fav_dog(f_d, df=df):
+    if f_d == 'f':
+        df2 = df.loc(df['money_line_close'] < 0)
+        return df2
+    else:
+        df2 = df.loc(df['money_line_close'] > 0)
+        return df2
+
+
 if __name__ == '__main__':
-    print(date_range)
+    print()
