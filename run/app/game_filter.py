7ae1c76d75f36167626141b5ff249889aa2b0409
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-05-22 11:32:54 -0400

    filter class

--- run/app/game_filter.py
+++ run/app/game_filter.py
@@ -8,23 +8,22 @@ csvfile = os.path.join(os.path.dirname(__file__), '..', 'setup', 'data', 'baseba
 df = pd.read_csv(csvfile, low_memory=False)
 df = fix_df_types(df)
 
-def date_range(df=df, start_date=date(2018,1,1), end_date=date(2019,12,31)):
-    df['date'] = pd.to_datetime(df['date'])
-    df2 = df[df['date'].isin(pd.date_range(start_date,end_date))]
-    return df2
-
-def home_away(h_a, df=df):
-    df2 = df.loc(df['visitor_home'] == h_a)
-    return df2
-
-def fav_dog(f_d, df=df):
-    if f_d == 'f':
-        df2 = df.loc(df['money_line_close'] < 0)
+class Filter:
+    
+    def __init__(self, start_date, end_date, h_a, f_d):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.h_a = h_a
+        self.f_d = f_d
+        
+    def get_df(self):
+        df['date'] = pd.to_datetime(df['date'])
+        df2 = df[df['date'].isin(pd.date_range(self.start_date,self.end_date))]
+        if self.f_d == 'f':
+            df2 = df.loc[(df['visitor_home'] == self.h_a) & (df['money_line_close'] < 0)]
+        else:
+            df2 = df.loc[(df['visitor_home'] == self.h_a) & (df['money_line_close'] > 0)]
         return df2
-    else:
-        df2 = df.loc(df['money_line_close'] > 0)
-        return df2
-
 
 if __name__ == '__main__':
     print()
