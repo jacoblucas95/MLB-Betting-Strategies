d30c8b2e75760903f1e1ca07d3c3154f128291f3
James Roth <james@Jamess-MacBook-Pro.local>
2019-05-22 15:45:21 -0400

    additional folder structure changes

--- run/flask_app/__init__.py
+++ run/flask_app/__init__.py
@@ -1,4 +1,4 @@
 from flask_app.handler import get_game_data, game_sequence, fix_df_types, df
 from flask_app.game import create_betting_results
-# from app.game_filter import date_range
+from flask_app.game_filter import Filter
 from flask_app.strategies import home_team, visitor_team, overs, underdogs, unders, favorites
