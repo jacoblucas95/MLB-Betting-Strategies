18a3c60ffd6e017a808dd15a21e1d826394d2029
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-05-28 17:14:45 -0400

    shtuff

--- run/flask_app/routes.py
+++ run/flask_app/routes.py
@@ -12,37 +12,6 @@ from flask_app.game_filter import Filter
 # from app.game_filter import date_range
 
 
-@app.route('/')
-def root():
-	return jsonify({
-		"game_1":{
-			"home_team":"Home Team1",
-			"away_team":"Away Team1",
-			"location":"City1",
-			"date":"1499817600",
-			"final_score":"69",
-			"odds":"6:9",
-			"open_close_ratio":"9:6",
-			"pitcher":{
-				"name":"Mikey Mike",
-				"hand":"R"
-				}
-		},
-		"game_2":{
-			"home_team":"Home Team2",
-			"away_team":"Away Team2",
-			"location":"City2",
-			"date":"1531353600",
-			"final_score":"420",
-			"odds":"4:20",
-			"open_close_ratio":"4:20",
-			"pitcher":{
-				"name":"Bobby Bob",
-				"hand":"L"
-				}
-		}
-	})
-
 @app.route('/test',  methods=['GET'])
 def test():
 	if request.method == 'GET':
